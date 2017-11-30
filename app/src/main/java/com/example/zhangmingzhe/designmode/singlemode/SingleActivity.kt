7fd29fc2d03e80ecee54b0046f82180b8a07296e
张明哲 <1084904209@qq.com>
2017-11-30 18:32:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zhangmingzhe/designmode/singlemode/SingleActivity.kt
@@ -0,0 +1,13 @@
+package com.example.zhangmingzhe.designmode.singlemode
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.zhangmingzhe.designmode.R
+
+class SingleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single)
+    }
+}
